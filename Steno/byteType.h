d45864b745141271e80fabd88308423a0350724a
Simone Cisca <simone.cisca@studenti.unicam.it>
2022-06-04 21:27:26 +0200

    [ADD] - Directory: Steno In steno: [ADD] -byteType.h: Creating bytes using an unsigned char -main.c -utilities: Essential methods, potentially others will be added

---
+++ Steno/byteType.h
@@ -0,0 +1,5 @@
+#ifndef OSPROJECT_BYTETYPE_H
+#define OSPROJECT_BYTETYPE_H
+    typedef unsigned char byte;
+    //Per evitare ambiguità si è deciso di "creare" byte, di fatto un char corrisponde a un byte.
+#endif //OSPROJECT_BYTETYPE_H
---
+++ Steno/utilities.c
@@ -0,0 +1,42 @@
+#include "utilities.h"
+
+/**
+ * This method get the length of File.
+ * @param f as File.
+ * @return the length.
+ */
+long getFileLength(FILE *f){
+    fseek(f, 0, SEEK_END);
+    long length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    return length;
+}
+
+/**
+ * This method check error on file.
+ * @param f as File to check.
+ * @return 0 if it don't find an error.
+ *         1 else.
+ */
+int fileErrorCheck(FILE *f){
+    if(ferror(f)){
+        perror("Error during file operation.");
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * This method reads the next byte if possible.
+ * @param B as actual Byte.
+ * @param f as File.
+ * @return -1 if it can't read another byte because B is last.
+ *         0 if it don't find an error.
+ *         1 else.
+ */
+int readNextByte(byte *B, FILE *f){
+    if(fread(B, sizeof(unsigned char), 1, f) == 0)
+        return -1;
+
+    return fileErrorCheck(f);
+}
