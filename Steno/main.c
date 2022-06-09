2e7aff56bc02e375c6fb8df15d50743e4b0ee99f
SpaceCowboyS01 <simone.cisca@studenti.unicam.it>
2022-06-09 16:40:25 +0200

    [UPDATE]

--- Steno/main.c
+++ Steno/main.c
@@ -1,7 +1,4 @@
-// Created by @SpaceCowboyS01 on 04/06/2022.
-
 #include <stdio.h>
-
 int main(){
 
     return 0;
--- Steno/utilities.h
+++ Steno/utilities.h
@@ -1,31 +1,31 @@
 #ifndef OSPROJECT_UTILITIES_H
-#define OSPROJECT_UTILITIES_H
+    #define OSPROJECT_UTILITIES_H
 
-#include <stdio.h>
-#include "byte_type.h"
+    #include <stdio.h>
+    #include "byteType.h"
 
-/**
- * This method get the length of File.
- * @param f as File.
- * @return the length.
- */
-long getFileLength(FILE *f);
+    /**
+     * This method get the length of File.
+     * @param f as File.
+     * @return the length.
+     */
+    long getFileLength(FILE *f);
 
-/**
- * This method check error on file.
- * @param f as File to check.
- * @return 0 if it don't find an error.
- *         1 else.
- */
-int fileErrorCheck(FILE *f);
+    /**
+     * This method check error on file.
+     * @param f as File to check.
+     * @return 0 if it don't find an error.
+     *         1 else.
+     */
+    int fileErrorCheck(FILE *f);
 
-/**
- * This method reads the next byte if possible.
- * @param B as actual Byte.
- * @param f as File.
- * @return -1 if it can't read another byte because B is last.
- *         0 if it don't find an error.
- *         1 else.
- */
-int readNextByte(byte* B, FILE *f);
+    /**
+     * This method reads the next byte if possible.
+     * @param B as actual Byte.
+     * @param f as File.
+     * @return -1 if it can't read another byte because B is last.
+     *         0 if it don't find an error.
+     *         1 else.
+     */
+    int readNextByte(byte* B, FILE *f);
 #endif //OSPROJECT_UTILITIES_H
