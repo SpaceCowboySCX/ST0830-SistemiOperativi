ba74f0f676aea3b62e2beb3ca0446420866e38db
mrentity303 <patryk.bialowas@studenti.unicam.it>
2022-06-14 01:44:11 -0700

    [UPDATE] main test split

--- Steno/main.c
+++ Steno/main.c
@@ -1,4 +1,17 @@
-#include "commandList.h"
+#include "utilities.h"
+#include "string.h"
 int main(){
+    char str[255];
+    printf("Inserire i file separati da uno spazio\n");
+    scanf("%s\n", str);
+//    printf("stampa: %s", str);
+    char array;
+    array = split(str);
+    printf("Inserire i file separati da uno spazio\n");
+    for(int i = 0; i < 10; i=i+1)
+    {
+        printf("stampa: %s", &array[i]);
+    }
+
     return 0;
 }
