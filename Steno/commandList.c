5901ac8e38f9911e09689545a34f11fa753d9de2
SpaceCowboyS01 <simone.cisca@studenti.unicam.it>
2022-06-14 10:41:38 +0200

    [UPDATE] Implements showContent

--- Steno/commandList.c
+++ Steno/commandList.c
@@ -5,10 +5,36 @@ void help(){
     printf("Type 2) showContent:\tShow the content of current directory.\n");
     printf("Type 3) changeDirectory:\tChange the current directory to the one passed by param.\n");
     printf("Type 4) createTXT:\tCreate a new txt file in the current directory.\n");
-    printf("Type 5) checkTXT:\tPrint a TXT file in the current directory.\n");
+    printf("Type 5) printTXT:\tPrint a TXT file in the current directory.\n");
     printf("Type 6) executeHiding:\tExecute hiding Algorithm");
     printf("Type 7) executeUnveiling:\tExecute unveiling Algorithm");
     printf("Type 0) terminate:\tTerminate the program execution.\n");
+}
+
+void currentPath(){
+    char current_path[MAX_PATH];
+    if(getcwd(current_path, sizeof(current_path)) != NULL)
+        printf("%s", current_path);
+    else
+        perror("Error: getcwd() error.\n");
+}
+
+int showContent(){
+    DIR *directory;
+    struct dirent *entry;
+    int number_of_files = 0;
+
+    directory = opendir(".");
+    if(directory == NULL){
+        perror("Unable to read this directory.\n");
+        return 1;
+    }
+    while ((entry = readdir(directory))){
+        number_of_files++;
+        printf("File %2d:\t%s\n", number_of_files, entry->d_name);
+    }
+    closedir(directory);
+    return 0;
 }
 
 int changeDirectory(const char *PATH){
@@ -18,10 +44,6 @@ int changeDirectory(const char *PATH){
     return chdir(PATH);
 }
 
-void terminate(){
-    printf("Thanks!");
-    _Exit(0);
-}
 
 void createTXT(){
     char string[MAX_LENGTH_STRING];
@@ -36,38 +58,51 @@ void createTXT(){
 }
 
 void printTXT(){
-    FILE *fileToPrint;
-    char string[MAX_LENGTH_STRING], characterToPrint;
+    FILE *file_to_print;
+    char string[MAX_LENGTH_STRING], character_to_print;
 
     printf("Name's file:\t");
     scanf("%s", string);
 
     //TODO check if exist
     if(extensionFileCheck(string) != 1) {
-        fileToPrint = fopen(string, "r");
-        characterToPrint = fgetc(fileToPrint);
+        file_to_print = fopen(string, "r");
+        character_to_print = fgetc(file_to_print);
 
-        while(characterToPrint != EOF) {
-            printf("%c", characterToPrint);
-            characterToPrint = fgetc(fileToPrint);
+        while(character_to_print != EOF) {
+            printf("%c", character_to_print);
+            character_to_print = fgetc(file_to_print);
         }
-        fclose(fileToPrint);
+        fclose(file_to_print);
     }
 }
 
-
-void currentPath(){
-    char currentPath[MAX_PATH];
-    if(getcwd(currentPath, sizeof(currentPath)) != NULL)
-        printf("%s", currentPath);
-    else
-        perror("Error: getcwd() error.\n");
-}
-
-void executeHiding(){
+int executeHiding(char name_file_txt[MAX_LENGTH_STRING], char name_file_immagine[MAX_LENGTH_STRING]){
     /*Apertura file
     * Controllo lunghezza immagine
     * controllo lunghezza testo
     * !deve essere (Lunghezza)/8 < Lunghezza immagine!
     */
+
+    FILE *file_txt;
+    FILE *file_img;
+    FILE *file_output;
+
+    return 0;
+}
+
+
+int executeUnveiling(char name_file_immagine[MAX_LENGTH_STRING]){
+
+    FILE *file_txt;
+    FILE *file_img;
+    FILE *file_output;
+
+    return 0;
+}
+
+
+void terminate(){
+    printf("Thanks!");
+    _Exit(0);
 }
--- Steno/commandList.h
+++ Steno/commandList.h
@@ -4,6 +4,7 @@
     #include "utilities.h"
     #include <stdlib.h>
     #include <unistd.h>
+    #include <dirent.h>
 
     #define MAX_PATH 200
 
@@ -13,15 +14,22 @@
     void help();
 
     /**
+     * This method return the current path.
+     * @return
+     */
+    void currentPath();
+
+    /**
+     * This method show the content of the current path.
+     * @return
+     */
+    int showContent();
+
+    /**
      * This method change the current directory to the one passed by param.
      * @param PATH
      */
     int changeDirectory(const char *PATH);
-
-    /**
-     * This method terminate the program execution.
-     */
-    void terminate();
 
     /**
      * This method create a new txt file in the current directory.
@@ -35,12 +43,24 @@
     void printTXT(const char *txtName);
 
     /**
-     * This method return the current path.
-     * @return
+     * This method read file and execute the algorithm for Hiding.
+     * @param nome_file_txt
+     * @param nome_file_immagine
+     * @return  0 - success
+     *          1 - fail
      */
-    void currentPath();
+    int executeHiding(char name_file_txt[MAX_LENGTH_STRING], char name_file_immagine[MAX_LENGTH_STRING]);
 
-    void executeHiding();
+    /**
+     * This method read file and execute the algorithm for unveiling.
+     * @param nome_file_immagine
+     * @return  0 - success
+     *          1 - fail
+     */
+    int executeUnveiling(char name_file_immagine[MAX_LENGTH_STRING]);
 
-    void executeUnveiling();
+    /**
+     * This method terminate the program execution.
+     */
+    void terminate();
 #endif //ST0830_O_S_PROJECT_COMMANDLIST_H
