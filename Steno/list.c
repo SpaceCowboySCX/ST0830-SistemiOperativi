ca8cb11ccad80b5294a255b6462ffa048520dd05
mrentity303 <patryk.bialowas@studenti.unicam.it>
2022-06-15 14:01:16 -0700

    [UPDATE] modify list.c and list.h

--- Steno/list.c
+++ Steno/list.c
@@ -1,41 +1,57 @@
 #include "list.h"
 
-Node *first_element;
-Node *last_element;
-
-Node* insert(char string_to_insert[MAX_LENGTH_STRING])
+Node* insert(Node* first_element, char name_file_txt[], char name_file_bmp[])
 {
-    if(last_element == NULL)
+    if(first_element == NULL)
     {
-        last_element = (Node*)malloc(sizeof (Node));
-        strcpy(last_element->str, string_to_insert);
-        first_element = last_element;
-        //first_element->next = last_element;
+        first_element = (Node*)malloc(sizeof (Node));
+        strcpy(first_element->name_file_txt, name_file_txt);
+        strcpy(first_element->name_file_bmp, name_file_bmp);
+        return first_element;
     }
-    else {
-        Node *new_node = (Node *)malloc(sizeof(Node));
-        strcpy(new_node->str, string_to_insert);
-        last_element->next = new_node;
-        if(first_element->next == NULL)first_element->next = last_element;
-        last_element = last_element->next;
+    else
+    {
+        if(first_element->next == NULL)
+        {
+            first_element->next = (Node*)malloc(sizeof (Node));
+            strcpy(first_element->next->name_file_txt, name_file_txt);
+            strcpy(first_element->next->name_file_bmp, name_file_bmp);
+            return first_element;
+        }
+        Node* tmp = first_element;
+        while(tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+        tmp->next = (Node*)malloc(sizeof (Node));
+        strcpy(tmp->next->name_file_txt, name_file_txt);
+        strcpy(tmp->next->name_file_bmp, name_file_bmp);
+        return first_element;
     }
-    return first_element;
 }
 
-Node* getFirstElement()
+char* getNameFileTxt(Node* first_element)
 {
-    return first_element;
+    if(first_element != NULL) {
+        return first_element->name_file_txt;
+    }
+    return NULL;
 }
 
-char* readAndRemove()
+char* getNameFileBmp(Node* first_element)
 {
-    static char result[MAX_LENGTH_STRING];
+    if(first_element != NULL) {
+        return first_element->name_file_bmp;
+    }
+    return NULL;
+}
+
+Node* removeElement(Node* first_element)
+{
     if(first_element != NULL)
     {
-        //result = (char *) firstElement->str;
-        strcpy(result, first_element->str);
         first_element = first_element->next;
-        return result;
+        return first_element;
     }
     return NULL;
 }
@@ -45,15 +61,31 @@ Node* getNext(Node* element)
     return element->next;
 }
 
-char* getStringOfElement(Node* element)
-{
-    return element->str;
-}
 
-Node* getLastElement()
+
+Node* getLastElement(Node* first_element)
 {
-    return last_element;
+    Node* tmp = first_element;
+    while(tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    return tmp;
 }
+//ESEMPIO USO LISTA
+//    Node* list = NULL;
+//    list = insert(list, "1", "2");
+//    list = insert(list, "3", "4");
+//    list = insert(list, "5", "6");
+//    list = insert(list, "7", "8");
+//
+//    while(list != NULL)
+//    {
+//        printf("\nStampo: %s", getNameFileTxt(list));
+//        printf("\nStampo: %s", getNameFileBmp(list));
+//        printf("\n-----------------------------------------------------------------");
+//        list = removeElement(list);
+//    }
 
 
 
--- Steno/list.h
+++ Steno/list.h
@@ -5,38 +5,39 @@
 #include "stdlib.h"
 
 struct Node{
-    char str[MAX_LENGTH_STRING];
+    char name_file_txt[MAX_LENGTH_STRING];
+    char name_file_bmp[MAX_LENGTH_STRING];
     struct Node *next;
 } typedef Node;
 
-    /**
-     * This method put an element string on list.
-     * @param
-     *                  array of char.
-     * @return
-     *                  firstElement of list
-     */
-Node* insert(char array[]);
-
-    /**
-     * This method return firstElement on the list.
-     * @return
-     *                  firstElement of list
-     */
-Node *getFirstElement();
 
 /**
- * This method readAndRemove 1 element on the list.
- * @return
- *                     string value of element.
- */
-char* readAndRemove();
+* This method put an element string on list.
+* @param first_element
+*                  first element of the list.
+* @param string
+*                  name_file_txt.
+* @param string
+*                  name_file_bmp.
+* @return
+*                  firstElement of list.
+*/
+Node* insert(Node* first_element, char name_file_txt[], char name_file_bmp[]);
+
+/**
+* This method remove an element on list.
+* @param first_element
+*                  name_file_txt.
+* @return
+*                  return first element of the list after remove list.
+*/
+Node* removeElement(Node* first_element);
 
 
 /**
- * This method return new of the list element.
+ * This method return next of the list element.
  * @param element
- *                  element of the next.
+ *                  element.
  * @return
  *                  next node of element.
  */
@@ -44,20 +45,32 @@ Node* getNext(Node* element);
 
 
 /**
- * This method return an string of this element.
+ * This method return an name of file txt of this element.
  * @param element
  *                  element of the list.
  * @return
- *                  string of this element
+ *                  string name of file txt of this element.
  */
-char* getStringOfElement(Node* element);
+char* getNameFileTxt(Node* element);
+
+
+/**
+ * This method return an name of file bmp of this element.
+ * @param element
+ *                  element of the list.
+ * @return
+ *                  string name of file bmp of this element.
+ */
+char* getNameFileBmp(Node* element);
 
 
 /**
  * This method return lastElement on the list.
+ * @param first_element
+ *                  first element of the list.
  * @return
- *                  firstElement of list
+ *                  firstElement of list.
  */
-Node* getLastElement();
+Node* getLastElement(Node* first_element);
 
 #endif //ST0830_O_S_PROJECT_LIST_H
