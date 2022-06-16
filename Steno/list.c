6fb572487033b1b4d87c6d9b7da7a71dc77b6d3a
SpaceCowboyS01 <simone.cisca@studenti.unicam.it>
2022-06-16 14:12:37 +0200

    [UPDATE] Simpled insert method

--- Steno/list.c
+++ Steno/list.c
@@ -1,84 +1,63 @@
 #include "list.h"
 
-Node* insert(Node* first_element, char name_file_txt[], char name_file_bmp[])
-{
-    if(first_element == NULL)
-    {
-        first_element = (Node*)malloc(sizeof (Node));
+Node *insert(Node *first_element, char name_file_txt[], char name_file_bmp[]) {
+    if (first_element == NULL) {
+        first_element = (Node *) malloc(sizeof(Node));
         strcpy(first_element->name_file_txt, name_file_txt);
         strcpy(first_element->name_file_bmp, name_file_bmp);
         return first_element;
-    }
-    else
-    {
-        if(first_element->next == NULL)
-        {
-            first_element->next = (Node*)malloc(sizeof (Node));
-            strcpy(first_element->next->name_file_txt, name_file_txt);
-            strcpy(first_element->next->name_file_bmp, name_file_bmp);
-            return first_element;
-        }
-        Node* tmp = first_element;
-        while(tmp->next != NULL)
-        {
+    } else {
+        Node *tmp = first_element;
+        while (tmp->next != NULL)
             tmp = tmp->next;
-        }
-        tmp->next = (Node*)malloc(sizeof (Node));
+
+        tmp->next = (Node *) malloc(sizeof(Node));
         strcpy(tmp->next->name_file_txt, name_file_txt);
         strcpy(tmp->next->name_file_bmp, name_file_bmp);
         return first_element;
     }
 }
 
-char* getNameFileTxt(Node* first_element)
-{
-    if(first_element != NULL) {
+char *getNameFileTxt(Node *first_element) {
+    if (first_element != NULL) {
         return first_element->name_file_txt;
     }
     return NULL;
 }
 
-char* getNameFileBmp(Node* first_element)
-{
-    if(first_element != NULL) {
+char *getNameFileBmp(Node *first_element) {
+    if (first_element != NULL) {
         return first_element->name_file_bmp;
     }
     return NULL;
 }
 
-Node* removeElement(Node* first_element)
-{
-    if(first_element != NULL){
+Node *removeElement(Node *first_element) {
+    if (first_element != NULL) {
         first_element = first_element->next;
         return first_element;
     }
     return NULL;
 }
 
-Node* getNext(Node* element)
-{
+Node *getNext(Node *element) {
     return element->next;
 }
 
-
-
-Node* getLastElement(Node* first_element)
-{
-    Node* tmp = first_element;
-    while(tmp->next != NULL)
-    {
+Node *getLastElement(Node *first_element) {
+    Node *tmp = first_element;
+    while (tmp->next != NULL) {
         tmp = tmp->next;
     }
     return tmp;
 }
 
-void printList(Node *element)
-{
-    if(element ==NULL)
+void printList(Node *element) {
+    if (element == NULL)
         return;
     printf("\nStampo: %s", getNameFileTxt(element));
     printf("\nStampo: %s", getNameFileBmp(element));
-    if(getNext(element) != NULL)
+    if (getNext(element) != NULL)
         printList(getNext(element));
 }
 //ESEMPIO USO LISTA
